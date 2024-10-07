0300e9ae419aa0f50194e99c8133b7da7863fe7d
Luana Lamonica <luanalcbertoli11@gmail.com>
2024-10-07 19:09:57 -0300

    feat: login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -112,16 +112,27 @@
             </div>
         </nav>
     </header>
-
-    <div class="login-container">
-        <h1>Sign In</h1>
-        <input type="email" placeholder="E-mail">
-        <input type="password" placeholder="Password">
-        <button>Enter</button>
-        <div class="footer-links">
-           <a href="{{ url('/register') }}">Register</a>
-        </div>
-    </div>
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="login-container">
+                <h1>Sign In</h1>
+                <input type="email" placeholder="E-mail" name="email">
+                <input type="password" placeholder="Password" name="password">
+                <button type="submit">Enter</button>
+                <div class="footer-links">
+                    <a href="{{ url('/register') }}">Register</a>
+                </div>
+            </div>
+        </form>
+        @if ($errors->any())
+            <div style="background-color: white !important; color: green !important">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 </body>
 </html>
         
